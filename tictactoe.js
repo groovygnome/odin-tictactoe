7d389cb440dd1e26d4808bcec3b378cc3e0065f0
groovygnome <jrp810@proton.me>
2025-03-23 12:00:00 -0400

    add createBoard, createPlayer, createTile modules, add some game logic

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,82 @@
-var tictactoe = (function() {
+function createPlayer(name, move, turn) {
+
+    const getName = () => name;
+    const getMove = () => move;
+    const getTurn = () => turn;
+    const changeTurn = () => turn = !turn;
+
+    return { getName, getMove, getTurn, changeTurn };
+}
+
+function createTile(index) {
+    let symbol = index;
+
+    const getSymbol = () => symbol;
+    const claimTile = (newSymbol) => {
+        if (symbol == index) {
+            symbol = newSymbol;
+        }
+    };
+
+    return { index, getSymbol, claimTile };
+
+}
+
+function createGameBoard() {
+    let tiles = [];
+
+    for (let i = 0; i < 9; i++) {
+        tiles.push(createTile(i));
+    }
+
+    /** tiles
+     *  [ 0 , 1 , 2
+     *    3 , 4 , 5
+     *    6 , 7 , 8 ]
+    **/
+    const checkRound = () => {
+        if (
+            (tiles[0].getSymbol() == tiles[1].getSymbol() == tiles[2].getSymbol()) ||
+            (tiles[3].getSymbol() == tiles[4].getSymbol() == tiles[5].getSymbol()) ||
+            (tiles[6].getSymbol() == tiles[7].getSymbol() == tiles[8].getSymbol()) ||
+            (tiles[0].getSymbol() == tiles[3].getSymbol() == tiles[6].getSymbol()) ||
+            (tiles[1].getSymbol() == tiles[4].getSymbol() == tiles[7].getSymbol()) ||
+            (tiles[2].getSymbol() == tiles[5].getSymbol() == tiles[8].getSymbol()) ||
+            (tiles[0].getSymbol() == tiles[4].getSymbol() == tiles[8].getSymbol()) ||
+            (tiles[2].getSymbol() == tiles[4].getSymbol() == tiles[6].getSymbol())
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return { tiles, checkRound }
+}
+
+let gameBoard = (function() {
+    let playerOne = createPlayer('jeff', 'X', true);
+    let playerTwo = createPlayer('jill', 'O', false);
+
+    let board = createGameBoard();
+
+    const playTurn = (tile) => {
+        let symbol;
+        playerOne.getTurn() ? symbol = playerOne.getMove() : symbol = playerTwo.getMove();
+        board.tiles[tile].claimTile(symbol);
+
+        console.log(board.checkRound());
+
+
+        playerOne.changeTurn();
+        playerTwo.changeTurn();
+    }
+
+    const displayBoard = () => {
+        console.log();
+
+    }
+
+    return { playerOne, playerTwo, board, playTurn };
 
 })();
