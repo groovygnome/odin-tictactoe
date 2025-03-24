3fed9318a2dcc6be7eefe8bf438ef11c238bcfe7
groovygnome <jrp810@proton.me>
2025-03-24 12:00:00 -0400

    finish game logic, implement game on DOM

--- tictactoe.js
+++ tictactoe.js
@@ -11,10 +11,13 @@ function createPlayer(name, move, turn) {
 function createTile(index) {
     let symbol = index;
 
-    const getSymbol = () => symbol;
+    const getSymbol = () => {
+        return symbol;
+    };
     const claimTile = (newSymbol) => {
         if (symbol == index) {
             symbol = newSymbol;
+            return true;
         }
     };
 
@@ -34,49 +37,107 @@ function createGameBoard() {
      *    3 , 4 , 5
      *    6 , 7 , 8 ]
     **/
-    const checkRound = () => {
-        if (
-            (tiles[0].getSymbol() == tiles[1].getSymbol() == tiles[2].getSymbol()) ||
-            (tiles[3].getSymbol() == tiles[4].getSymbol() == tiles[5].getSymbol()) ||
-            (tiles[6].getSymbol() == tiles[7].getSymbol() == tiles[8].getSymbol()) ||
-            (tiles[0].getSymbol() == tiles[3].getSymbol() == tiles[6].getSymbol()) ||
-            (tiles[1].getSymbol() == tiles[4].getSymbol() == tiles[7].getSymbol()) ||
-            (tiles[2].getSymbol() == tiles[5].getSymbol() == tiles[8].getSymbol()) ||
-            (tiles[0].getSymbol() == tiles[4].getSymbol() == tiles[8].getSymbol()) ||
-            (tiles[2].getSymbol() == tiles[4].getSymbol() == tiles[6].getSymbol())
-        ) {
-            return true;
-        } else {
-            return false;
-        }
+
+
+    const checkRound = () => (
+        (tiles[0].getSymbol() === tiles[1].getSymbol() && tiles[1].getSymbol() === tiles[2].getSymbol()) ||
+        (tiles[3].getSymbol() === tiles[4].getSymbol() && tiles[4].getSymbol() === tiles[5].getSymbol()) ||
+        (tiles[6].getSymbol() === tiles[7].getSymbol() && tiles[7].getSymbol() === tiles[8].getSymbol()) ||
+        (tiles[0].getSymbol() === tiles[3].getSymbol() && tiles[3].getSymbol() === tiles[6].getSymbol()) ||
+        (tiles[1].getSymbol() === tiles[4].getSymbol() && tiles[4].getSymbol() === tiles[7].getSymbol()) ||
+        (tiles[2].getSymbol() === tiles[5].getSymbol() && tiles[5].getSymbol() === tiles[8].getSymbol()) ||
+        (tiles[0].getSymbol() === tiles[4].getSymbol() && tiles[4].getSymbol() === tiles[8].getSymbol()) ||
+        (tiles[2].getSymbol() === tiles[4].getSymbol() && tiles[4].getSymbol() === tiles[6].getSymbol())
+    );
+
+    const displayBoard = () => {
+        console.log(tiles[0].getSymbol() + ' | ' + tiles[1].getSymbol() + ' | ' + tiles[2].getSymbol());
+        console.log('--------');
+        console.log(tiles[3].getSymbol() + ' | ' + tiles[4].getSymbol() + ' | ' + tiles[5].getSymbol());
+        console.log('--------');
+        console.log(tiles[6].getSymbol() + ' | ' + tiles[7].getSymbol() + ' | ' + tiles[8].getSymbol());
     }
 
-    return { tiles, checkRound }
+    return { tiles, checkRound, displayBoard }
 }
 
-let gameBoard = (function() {
-    let playerOne = createPlayer('jeff', 'X', true);
-    let playerTwo = createPlayer('jill', 'O', false);
+function createTicTacToe(playerOne, playerTwo) {
+    let inPlay = true;
+    let roundCounter = 0;
 
     let board = createGameBoard();
 
     const playTurn = (tile) => {
-        let symbol;
-        playerOne.getTurn() ? symbol = playerOne.getMove() : symbol = playerTwo.getMove();
-        board.tiles[tile].claimTile(symbol);
+        if (inPlay) {
+            roundCounter++;
+            let currPlayer;
+            playerOne.getTurn() ? currPlayer = playerOne : currPlayer = playerTwo;
+            let symbol = currPlayer.getMove();
+            board.tiles[tile].claimTile(symbol);
+            displayBoard();
 
-        console.log(board.checkRound());
-
-
-        playerOne.changeTurn();
-        playerTwo.changeTurn();
+            if (board.checkRound() || roundCounter == 9) {
+                if (roundCounter == 9) {
+                    console.log('Tie!')
+                } else {
+                    console.log(currPlayer.getName() + ' has won!');
+                }
+                inPlay = false;
+            } else {
+                playerOne.changeTurn();
+                playerTwo.changeTurn();
+            }
+        } else {
+            console.log("The game is over, you need to start a new one");
+        }
     }
 
     const displayBoard = () => {
-        console.log();
-
+        board.displayBoard();
     }
 
-    return { playerOne, playerTwo, board, playTurn };
+    const isInPlay = () => { return inPlay; }
 
+    return { board, playTurn, displayBoard, isInPlay };
+
+};
+
+const DOMGame = (function() {
+    let gameBoard;
+    const newGameBtn = document.querySelector('.new-game');
+    const newGameForm = document.querySelector('.game-form');
+    const closeBtn = document.querySelector('.close');
+    const submitBtn = document.querySelector('.submit');
+    newGameBtn.addEventListener('click', () => { newGameForm.show(); });
+    closeBtn.addEventListener('click', () => { newGameForm.close(); event.preventDefault(); });
+    submitBtn.addEventListener('click', () => {
+        newGameForm.close();
+        event.preventDefault();
+        let playerOne = createPlayer(document.querySelector('#p1name').value, document.querySelector('#p1symbol').value, true);
+        let playerTwo = createPlayer(document.querySelector('#p2name').value, document.querySelector('#p2symbol').value, false);
+        gameBoard = createTicTacToe(playerOne, playerTwo);
+        displayBoard();
+    });
+
+
+    const displayBoard = () => {
+        let display = document.createElement('div');
+        let container = document.querySelector('.container');
+        display.className = 'board';
+
+        let tiles = gameBoard.board.tiles;
+        for (let tile of tiles) {
+            let displayTile = document.createElement('button');
+            displayTile.className = 'tile'
+            displayTile.textContent = '';
+            displayTile.addEventListener('click', () => {
+                if (gameBoard.isInPlay()) {
+                    gameBoard.playTurn(tile.index);
+                    displayTile.textContent = tile.getSymbol();
+                }
+            });
+            display.appendChild(displayTile);
+        }
+        container.appendChild(display);
+    }
 })();
